8acdd91295813d723045eec598076d8b210eb3f5
gbswm0929 <gbswst0929@gmail.com>
2025-07-09 18:08:41 +0900

    수정3

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,43 @@
 import React from "react";
 import "./App.css";
-
+import Club from "./components/Club";
+import Notice from "./components/Notice";
+import Home from "./components/Home";
 import Contact from "./components/Contact";
 
 function App() {
   return (
     <div className="app">
+
       <header>
         <h1>🏫 우리 학교 홈페이지에 오신 것을 환영합니다!</h1>
       </header>
 
 
+      <section id="notice">
+        <Notice />
+      </section>
+
+      <section id="home">
+        <Home />
+      </section>
+
+      <section id="club">
+        <Club />
+      </section>
+
 
       <section id="contact">
         <Contact />
       </section>
-
+      
       <footer>
-        <p>© 2025 우리학교. All rights reserved.</p>
+        <p>© 2025 경북소프트웨어마이스터고등학교. All rights reserved.</p>
       </footer>
+
     </div>
   );
 }
+
 
 export default App;
