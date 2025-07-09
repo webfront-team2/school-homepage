99392f874bebb1eb275f000b20db4de3dc419da0
gbswm0929 <gbswst0929@gmail.com>
2025-07-09 18:06:09 +0900

    수정2

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,6 @@
 import React from "react";
 import "./App.css";
-// import Home from "./components/Home";
-// import Notice from "./components/Notice";
-// import Club from "./components/Club";
+
 import Contact from "./components/Contact";
 
 function App() {
@@ -12,17 +10,7 @@ function App() {
         <h1>🏫 우리 학교 홈페이지에 오신 것을 환영합니다!</h1>
       </header>
 
-      <section id="home">
-        {/* <Home /> */}
-      </section>
 
-      <section id="notice">
-        {/* <Notice /> */}
-      </section>
-
-      <section id="club">
-        {/* <Club /> */}
-      </section>
 
       <section id="contact">
         <Contact />
